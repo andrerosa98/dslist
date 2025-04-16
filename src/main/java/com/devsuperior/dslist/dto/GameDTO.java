bc7cec77f1a7299ae924c0f61c281288119020bf
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2025-04-16 14:12:03 -0300

    GET game by Id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+     public GameDTO(){
+    //Construtor vazio
+     }
+
+     public GameDTO(Game entity){
+         BeanUtils.copyProperties(entity, this);
+     }
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,12 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)//Assegura que o metodo não vai fazer alterações no banco de dados, ou seja, fica mais rápido
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+        return new GameDTO(result);
+    }
 
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
